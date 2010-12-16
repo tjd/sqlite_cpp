68d30411aa60512b5a49feb5b05efcfa2e6e664e
Toby Donaldson <tjdonaldson@gmail.com>
2010-12-15 23:55:34 -0800

    basic C++ sqlite db access working

--- topswops/sqlite/soci_demo.cpp
+++ topswops/sqlite/soci_demo.cpp
@@ -11,9 +11,45 @@ using namespace std;
 
 int main() {
   try {
-    cout << "This a test of soci" << endl;
-    session sql(sqlite3, "service=mydb user=john password=secret");
+    const string db_name("dbtest.db");
+    session sql(sqlite3, db_name);
 
+    cout << "Creating topswops table if necessary ..." << endl;
+    sql << "CREATE TABLE IF NOT EXISTS topswops(n INTEGER PRIMARY KEY, score INTEGER)";
+
+    cout << "Inserting some test values ..." << endl;
+    // BEGIN ... COMMIT perform the insertions in a single transaction
+    // (for speed)
+    sql.begin();
+    for(int i = 2; i <= 97; ++i) {
+      sql << "INSERT OR REPLACE INTO topswops(n, score) VALUES(" << i 
+          << ", 0)";
+    }
+    sql.commit();
+
+    cout << "Selecting and displaying contents of topswops table ..." << endl;
+    for(int i = 2; i <= 97; ++i) {
+      int score = -1;
+      sql << "SELECT score FROM topswops WHERE n = " << i, into(score);
+      cout << "n = " << i << ", score = " << score << endl;
+    }
+
+    //    vector<int> ns, scores;
+    //    sql << "select n from topswops", into(ns);
+    //sql << "select scores from topswops", into(scores);
+
+
+//     row r;
+//     sql << "select * from topswops", into(r);
+//     cout << r.size() << " rows retrieved" << endl;
+//     for(int i = 0; i < r.size(); ++i) {
+//       const column_properties& props = r.get_properties(i);
+//       cout << "properties: " << props.get_name() << endl;
+//       int n = r.get<int>(i);
+//       int score = r.get<int>(i);
+//       cout << "n = " << n << ", score = " << score << endl;
+//     }
+// 
   } catch (exception const &e) {
     cerr << "Error: " << e.what() << '\n';
   }
