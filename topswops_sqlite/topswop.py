405d9e8c3b6f37173bf01944dd2f844e7868bc75
Toby Donaldson <tjdonaldson@gmail.com>
2010-12-17 15:25:56 -0800

    migrated files to sqlite DB

--- topswops_sqlite/topswop.py
+++ topswops_sqlite/topswop.py
@@ -1,8 +1,10 @@
 # topswop.py
 
 import random
+import os
 import sys
 import sqlite3
+import re
 
 n_vals = [  2,  3,  5,  7, 11, 
            13, 17, 19, 23, 29, 
@@ -37,6 +39,31 @@ def perm_score(perm):
         count += 1
     return count
 
+def transfer_file_to_db():
+    """ One-time function for populating sqlite db with current best
+    scores.
+     "INSERT OR REPLACE INTO topswops(n, score, perm, date) VALUES(" 
+    """
+    #os.chdir('best_so_far')
+    conn = sqlite3.connect('best_so_far.db')
+    c = conn.cursor()
+    c.execute("CREATE TABLE topswops(n INTEGER PRIMARY KEY, score INTEGER, perm TEXT, date TEXT)")
+    for n in n_vals:
+        print 'n = %s' % n
+        f = open('topswop_num_%s.txt' % n, 'r')
+        score = int(f.next().strip())
+        #print 'score = %s' % score
+        s = f.next().strip()
+        #print 's = %s' % s
+        perm = [int(i) for i in s.split(', ')]
+        #print 'perm = %s' % perm
+        sql = "insert into topswops(n, score, perm, date) values(%s, %s, '%s', date('now'))" % (n, score, s)
+        print sql
+        c.execute(sql)
+        print
+    conn.commit()
+    conn.close()
+    check_db_okay()
 
 def get_db_row(n, dbname = 'best_so_far.db'):
     """ Retrieve the best score so far for n.
@@ -45,8 +72,48 @@ def get_db_row(n, dbname = 'best_so_far.db'):
     c = conn.cursor()
     c.execute('select * from topswops where n = %s' % n)
     return c.fetchone()
+
+def perm_str_to_list(s):
+    return [int(i) for i in s.split(', ')]
+
+def is_perm(lst):
+    cpy = lst[:]
+    cpy.sort()
+    return cpy == range(1, len(cpy) + 1)
+
+def check_db_okay(dbname = 'best_so_far.db'):
+    print 'Checking "%s" ...' % dbname,
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    c.execute('select * from topswops')
+    error_count = 0
+    for row in c:
+        n, score, perm, date = row
+        if n < 2:
+            print 'Error: n = %s is too small (n >= 2)' % n
+            error_count += 1
+        if score < 0:
+            print 'Error: score = %s is too small (score >= 0)' % score
+            error_count += 1
+        lst = perm_str_to_list(perm)
+        if not is_perm(lst):
+            print 'Error: not a perm: %s' % perm
+            error_count += 1
+        if score != perm_score(lst):
+            print 'Error: incorrect score for n = %s' % n
+            error_count += 1
+
+    if error_count == 0:
+        print 'done (no errors)'
+        return True
+    elif error_count == 1:
+        print 'done (1 error)'
+        return False
+    else:
+        print 'done (%s errors)' % error_count
+        return False
         
-        
+
 #def get_num_file(n):
 #     assert n in n_vals
 #     f = open(make_file_name(n), 'r')
@@ -63,6 +130,9 @@ def gen_num_report(dbname = 'best_so_far.db'):
                                         perm TEXT,
                                         date TEXT)
     """
+    if not check_db_okay():
+        print 'Report not generated: DB has errors'
+        return
     conn = sqlite3.connect(dbname)
     c = conn.cursor()
     c.execute('select * from topswops')
@@ -102,5 +172,7 @@ if __name__ == '__main__':
         gen_num_report()
     elif params[0] == 'scores':
         list_current_best_scores()
-
-
+    elif params[0] == 'check':
+        check_db_okay()
+#    elif params[0] == 'transfer':
+#        transfer_file_to_db()
